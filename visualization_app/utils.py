c4839ebdb7f422f95ce7a444d9ae1961885f2a49
Muhammad Umer Saeed <muhammad.umer@ginkgoretail.com>
2023-02-03 18:46:38 +0500

    comming the work done till now

---
+++ visualization_app/utils.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import gc
+import base64
+from io import BytesIO
+
+def 
---
+++ visualization_app/views.py
@@ -0,0 +1,8 @@
+#from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    
+    #return HttpResponse("Hello, Django!")
+    print('this is print in views.py')
+    return render(request, 'index.html')
