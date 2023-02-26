a12c53ffa893485ab46f97c3d4adc6ee3ab5f7b9
Muhammad Umer Saeed <muhammad.umer@ginkgoretail.com>
2023-02-26 18:55:16 +0500

    use post method form to send data to django views

--- visualization_app/urls.py
+++ visualization_app/urls.py
@@ -3,4 +3,5 @@ from visualization_app import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    # path("page/", views.home, name="visualization_page"),
 ]
--- visualization_app/views.py
+++ visualization_app/views.py
@@ -1,8 +1,28 @@
 #from django.http import HttpResponse
 from django.shortcuts import render
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import gc
+
+def read_dataset(num):
+    dataset = pd.read_csv('https://media.githubusercontent.com/media/M-Umr1/datasets/master/Processed_BigDataset_for_Visualization.csv', nrows=int(num))
+    is_read = 1
+    number_of_rows_read = num
+    return dataset, is_read, number_of_rows_read
+
 
 def home(request):
     #return HttpResponse("Hello, Django!")
-    print('this is print in views.py')
-    print(request)
+    var = None
+    if request.method == 'POST':
+        var = request.POST.get('number_of_rows','')
+    # df = pd.DataFrame()
+    try:
+        if var is not None:
+            df, is_read, number_of_rows_read = read_dataset(int(var))
+
+            print(df.head(5))
+    except Exception as e:
+        print(str(e))
     return render(request, 'index.html')
